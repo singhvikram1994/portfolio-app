b6b0414c8171f30d7f4c2c17f2ab9e77e81c5eec
Vikram Singh <singh.vikram1994@gmail.com>
2024-09-29 13:22:22 -0400

    Portfolio APP

---
+++ README.md
@@ -0,0 +1,39 @@
+### A Portfolio Web APP
+
+A Portfolio Sample Responsive Web Application
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run build
+npm run dev
+
+# or
+yarn build
+yarn dev
+
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## Technology Stack
+
+- Next.js
+- React
+- Media Query
+
+### Demo
+
+[Portfolio](https://portfolio-webapp.vercel.app/)
+
+### APP Screenshot
+
+#### Desktop View
+
+![Desktop View](desktop-view.png?raw=true "Desktop View")
+
+#### Mobile View
+
+![Mobile View](mobile-view.png?raw=true "Mobile View")
---
+++ components/Footer.js
@@ -0,0 +1,49 @@
+import style from "../styles/Footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div className={style.container}>
+      <div className={style.cardL}>
+        <h1 className={style.title}>AV0CAD0 CREATIVES.</h1>
+        <h1 className={style.linkTitle}>
+          <Link href="/contact" className={style.link} passHref>
+            <>
+              <span className={style.linkText}>WORK WITH US</span>
+              <Image
+                src={process.env.NEXT_PUBLIC_URL + "/img/link.png"}
+                width="40px"
+                height="40px"
+                alt=""
+              />
+            </>
+          </Link>
+        </h1>
+      </div>
+      <div className={style.cardS}>
+        <div className={style.cardItem}>
+          200 Kings Street,
+          <br /> CANADA
+        </div>
+        <div className={style.cardItem}>
+          consta@profolio-webapp.com
+          <br /> +1 458-365-1236
+        </div>
+      </div>
+      <div className={style.cardS}>
+        <div className={style.cardItem}>
+          FOLLOW US:
+          <br /> __FB __IN __BE __TW
+        </div>
+        <div className={style.cardItem}>
+          © 2022 PORTFOLIO INTERACTIVE,
+          <br />
+          ALL RIGHTS RESERVED
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
